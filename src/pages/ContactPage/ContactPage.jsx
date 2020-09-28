5d25213cce3540eabf07745799592eaa5c74ddee
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-09-27 18:17:18 -0600

    update dev page to include more info and a slider

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import styles from './ContactPage.module.css';
 import Footer from '../../components/Footer/Footer';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 class ContactPage extends Component {
     render() {
--- src/pages/DevPage/DevPage.jsx
+++ src/pages/DevPage/DevPage.jsx
@@ -3,11 +3,12 @@ import styles from './DevPage.module.css';
 import { FaHtml5, FaNode, FaReact, FaGitSquare, FaGithub } from 'react-icons/fa';
 import { SiCsswizardry, SiJavascript, SiPython, SiPostgresql} from 'react-icons/si';
 import { DiDjango, DiRuby } from 'react-icons/di';
+import { CarouselProvider, Slider, Slide, Image, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 import Footer from '../../components/Footer/Footer';
 
 const iconStyle = {
     color: '#61dafb',
-    padding: '10px'
   }
 
 class DevPage extends Component {
@@ -15,24 +16,47 @@ class DevPage extends Component {
         return(
             <div className={styles.DevPage}>
                 <div className={styles.Content}>
-                    SOME MORE STUFF ABOUT CODING HERE
-                    <div className={styles.Tech}>
-                        <FaHtml5 style={iconStyle}/>
-                        <SiCsswizardry style={iconStyle}/>
-                        <SiJavascript style={iconStyle}/>
-                        <FaNode style={iconStyle}/>
-                        {/* EXPRESS AND MONGODB */}
-                        <FaReact style={iconStyle}/>
-                        <SiPython style={iconStyle}/>
-                        <DiDjango style={iconStyle}/>
-                        <SiPostgresql style={iconStyle}/>
-                        <DiRuby style={iconStyle}/>
-                        <FaGitSquare style={iconStyle}/>
-                        <FaGithub style={iconStyle}/>
+                    <h3>Some Background</h3>
+                    <div className={styles.Job}>
+                        <p>My journey as a developer began in the spring of 2019, when I left my career as an academic and joined <a href="https://drizly.com/" target="_blank">Drizly</a>, where I currently work as an Implementation Engineer.</p>
+                        <p>In this role, I work on Drizly's inventory data extraction pipeline. This involves writing, editing, and installing batch scripts and their associated SQL queries to onboard new retail partners, providing technical support for existing retail partners, and writing, editing, and deploying parsers to interpret the extracted data.</p>
                     </div>
-                    USE A CAROUSEL FOR YOUR PROJECTS
-                    <Footer className={styles.Footer}/>
+                    <div className={styles.GA}>
+                        <p>I am also a recent graduate of <a href="https://generalassemb.ly/" target="_blank">General Assembly's</a> Software Engineering Immersive Remote Flex Program.
+                        This course provided 420+ hours of instruction in a wide variety of languages, frameworks, and technologies, such as:</p>
+                        <div className={styles.Tech}>
+                            <FaHtml5 style={iconStyle}/>
+                            <SiCsswizardry style={iconStyle}/>
+                            <SiJavascript style={iconStyle}/>
+                            <FaNode style={iconStyle}/>
+                            {/* EXPRESS AND MONGODB */}
+                            <FaReact style={iconStyle}/>
+                            <SiPython style={iconStyle}/>
+                            <DiDjango style={iconStyle}/>
+                            <SiPostgresql style={iconStyle}/>
+                            <DiRuby style={iconStyle}/>
+                            <FaGitSquare style={iconStyle}/>
+                            <FaGithub style={iconStyle}/>
+                        </div>
+                    </div>
+                    <h3>Recent Projects</h3>
+                    <div className={styles.Project}>
+
+                        <CarouselProvider 
+                            naturalSlideHeight={125}
+                            naturalSlideWidth={150}
+                            totalSlides={2}
+                        >
+                            <Slider>
+                                <Slide index={0}><a href="https://github.com/johnrobertmahlan/take-a-hike"><Image hasMasterSpinner={true} src={'https://i.imgur.com/JxAsbuK.jpg'}></Image></a></Slide>
+                                <Slide index={1}>SECOND SLIDE</Slide>
+                            </Slider>
+                            <ButtonBack>Back</ButtonBack>
+                            <ButtonNext>Next</ButtonNext>
+                        </CarouselProvider>
+                    </div>
                 </div>
+                <Footer className={styles.Footer}/>
             </div>
         )
     }
