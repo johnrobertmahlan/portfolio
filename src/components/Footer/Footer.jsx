78e1b0f34fc62c808409da368309b22e534e8116
johnrobertmahlan <johnrobertmahlan@gmail.com>
2020-09-29 17:10:30 -0600

    restyle about page and add link home to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { GrMail } from 'react-icons/gr';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
+import { AiFillHome } from 'react-icons/ai';
 import styles from './Footer.module.css';
 
 const iconStyle = {
@@ -13,6 +14,7 @@ const iconStyle = {
 const Footer = () => {
     return(
         <div className={styles.smIcons}>
+            <Link to="/" style={iconStyle}><AiFillHome /></Link>
             <a href="https://github.com/johnrobertmahlan" target="_blank" style={iconStyle}><FaGithub /></a>
             <a href="https://www.linkedin.com/in/johnrobertmahlan/" target="_blank" style={iconStyle}><FaLinkedin /></a>
             <Link to="/contact" style={iconStyle}><GrMail /></Link>
--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -8,32 +8,30 @@ class AboutPage extends Component {
     render() {
         return(
             <div className={styles.AboutPage}>
-                <div className={styles.Bio}>
-                    <Fade top>
+                <h1>John Robert Mahlan</h1>
+                <div className={styles.BioImage}>
+                    <Fade left>
                         <img className={styles.Profile} src="https://i.imgur.com/jIf1H2h.jpg?1"></img>
                     </Fade>
-                    <Fade bottom>
-                        <h5>
-                        I am a full stack web developer based in Denver. I am a fan of clean, crisp code and simple, elegant design, and I strive to achieve both in my work. I am also a philosopher who enjoys grappling with foundational questions about the nature of existence, goodness, and reason. I am a lifelong learner who is always looking for the next challenge.
-                        </h5>
-                    </Fade>
                     <Fade right>
+                    <div className={styles.BioContent}>
                         <h5>
-                        After earning my Ph.D. in philosophy and spending more than a decade teaching philosophy at the university level, I decided to bring my passion for teamwork, leadership, and clear communication to tech. I love working through thorny problems with my peers and sharing hard-won insights with others. I thrive in positive, fast-paced environments.
+                            I am a full-stack software engineer with a Ph.D. in philosophy, a humanist working in tech, a recovering academic and lifelong learner.
                         </h5>
-                    </Fade>
-                    <Fade left>
                         <h5>
-                        Originally from New York, I have also lived in Scotland, Wisconsin, Virginia, California, Egypt, and now Colorado. When I'm not working (or moving), I enjoy reading, listening to music, traveling, and - sometimes - hiking.
+                            I am an Implementation Engineer at <a href="https://drizly.com/" target="_blank">Drizly</a> and a recent graduate of <a href="https://generalassemb.ly/" target="_blank">General Assembly's</a> Software Engineering Immersive Remote Flex Program. Before this, I spent a decade teaching philosophy at the <a href="https://uwm.edu/" target="_blank">University of Wisconsin - Milwaukee</a>, the <a href="https://www.virginia.edu/" target="_blank">University of Virginia</a>, <a href="https://www.aucegypt.edu/home" target="_blank">The American University in Cairo</a>, the <a href="https://www.richmond.edu/" target="_blank">University of Richmond</a>, and the <a href="https://www.umw.edu/" target="_blank">University of Mary Washington</a>.
                         </h5>
-                    </Fade>
-                    <Fade bottom>
+                        <h5>
+                        Originally from New York, I have also lived in Scotland, Wisconsin, Virginia, California, Egypt, and now Colorado. When I'm not working (or moving), I enjoy reading, listening to music, traveling, and - sometimes - hiking. 
+                        </h5>
                         <div className={styles.ButtonField}>
-                        <Link to="/about/dev" className={styles.Button}>Development</Link>
-                        <Link to="/about/phil" className={styles.Button}>Philosophy</Link>
+                            <Link to="/about/dev" className={styles.Button}>Development</Link>
+                            <Link to="/about/phil" className={styles.Button}>Philosophy</Link>
                         </div>
+                    </div>
                     </Fade>
                 </div>
+
                 <Footer className={styles.Footer}/>
             </div>
         )
